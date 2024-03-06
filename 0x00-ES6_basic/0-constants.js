d5c11f85815eeb60a7ae98bc180c4681a2079657
kimathikim <briankimathi94@gmail.com>
2024-03-06 07:11:00 +0300

    arrow function

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,3 @@
-#!/usr/bin/node
-
-/**
- * Returns the task string.
- * @returns {string} The task string.
- */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -13,13 +7,9 @@ export function getLast() {
   return ' is okey';
 }
 
-/**
- * Concatenates the string 'But sometimes let' with the result of the getLast() function.
- *
- * @returns {string} The concatenated string.
- */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }
