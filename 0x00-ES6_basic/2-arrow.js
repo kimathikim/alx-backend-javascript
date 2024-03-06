428318a878b34f0ca5e520f076a4e68e5680a8a5
kimathikim <briankimathi94@gmail.com>
2024-03-06 07:43:25 +0300

    arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
-export default function getNeighborhoodList() {
-  this.sanFransiscoNeighborhoods = ['SOMA', 'Union Square'];
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-  self.sanFransiscoNeighborhoods.push(newNeighborhood);
-  return self.sanFransiscoNeighborhoods;
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
