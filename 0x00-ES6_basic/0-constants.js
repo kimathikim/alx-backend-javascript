5e9f04bc5ecc1033fd5f2687da5cb891c86992e3
kimathikim <briankimathi94@gmail.com>
2024-03-06 06:24:53 +0300

    use const and let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/**
+ * Returns the task string.
+ * @returns {string} The task string.
+ */
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+}
+
+export function getLast() {
+    return ' is okey';
+}
+
+/**
+ * Concatenates the string 'But sometimes let' with the result of the getLast() function.
+ * 
+ * @returns {string} The concatenated string.
+ */
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+    return combination;
+}
