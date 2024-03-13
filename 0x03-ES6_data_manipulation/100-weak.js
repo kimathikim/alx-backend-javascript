7cd7c481bcd1d01c6bcb45d9c5d802259fdb3f5b
Brian Kimathi M <briankimathi94@gmail.com>
2024-03-13 08:31:36 +0300

    weak

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,13 @@
-export default function queryAPI(endpoint) {
-    const weakMap = new WeakMap();
-    let c = weakMap.has(endpoint) ? weakMap.get(endpoint) : 0;
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+    let c = weakMap.get(endpoint) || 0;
+
     c += 1;
+
     weakMap.set(endpoint, c);
+
     if (c >= 5) throw Error('Endpoint load is high');
+
     return c;
 }
