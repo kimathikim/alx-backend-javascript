298aeb08bdd4d6ba6d15084a58c26c4a59b5f382
Brian Kimathi M <briankimathi94@gmail.com>
2024-03-13 06:14:25 +0300

    New chages

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,4 @@
 export function createIteratorObject(report) {
-  const report = {
-    department1: ['Employee1', 'Employee2'],
-    department2: ['Employee3', 'Employee4']
-  };
-
   const iterator = createIteratorObject(report);
 
   let result = iterator.next();
