925c038dcacc764bd7e0dbb05244c407e3e8de6d
Brian Kimathi M <briankimathi94@gmail.com>
2024-05-02 13:09:14 +0300

    match string

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,14 +4,18 @@ const sendPayment = require("./3-payment");
 const utils = require("./utils");
 
 describe("Compute the total amount", function () {
-  it("The stings should match", function () {
-    total = utils.calculateNumber("SUM", 100, 20);
-    res_string = `The total is: ${total}`;
+  it("should match the calculation with Utils.calculateNumber", function () {
+    const calculateNumberStub = sinon.stub(utils, "calculateNumber");
+    calculateNumberStub.withArgs("SUM", 100, 20).returns(120);
 
     const consoleSpy = sinon.spy(console, "log");
     sendPayment(100, 20);
-    expect(consoleSpy.calledWith(res_string)).to.be.true;
 
+    const res_string = `The total is: 120`;
+
+    expect(consoleSpy.calledWithExactly(res_string)).to.be.true;
+
+    calculateNumberStub.restore();
     consoleSpy.restore();
   });
 });
