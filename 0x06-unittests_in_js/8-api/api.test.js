b7b1d69621381c6ba686ac00f46a93f4ee486087
Brian Kimathi M <briankimathi94@gmail.com>
2024-05-02 23:13:37 +0300

    simple integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,21 +2,14 @@ const { expect } = require("chai");
 const request = require("request");
 
 describe("Index Page", () => {
-  const options = {
-    url: "http://localhost:7865",
-    method: "GET",
-  };
+  it("Status Code: 200 : text: Welcome to the payment system", (done) => {
+    const options = {
+      url: "http://localhost:7865",
+      method: "GET",
+    };
 
-  it("Status Code: 200 ", (done) => {
     request(options, (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-
-      done();
-    });
-  });
-
-  it("text: Welcome to the payment system", (done) => {
-    request(options, (error, response, body) => {
       expect(body).to.equal("Welcome to the payment system\n");
 
       done();
