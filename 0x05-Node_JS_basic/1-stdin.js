cf887d12060640b0d85ba86a772160caa640dca1
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-04 23:13:10 +0300

    write and read

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-
   if (name !== null) {
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
