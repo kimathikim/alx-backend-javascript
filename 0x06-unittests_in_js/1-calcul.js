70495cd7cdff85d22f18fa59158b161a9e9bb2e6
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-07 07:34:26 +0300

    change title

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,15 +1,20 @@
-const calculateNumber = (type, a, b) => {
-  if (type === "SUM") {
-    return Math.round(a) + Math.round(b);
-  } else if (type === "SUBTRACT") {
-    return Math.round(a) - Math.round(b);
-  } else if (type === "DIVIDE") {
-    if (Math.round(b) === 0) {
-      return "Error";
-    }
-    return Math.round(a) / Math.round(b);
-  } else {
-    throw new Error("Type must be SUM, SUBTRACT or DIVIDE");
+function calculateNumber(type, a, b) {
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
+
+  switch (type) {
+    case "SUM":
+      return roundA + roundB;
+    case "SUBTRACT":
+      return roundA - roundB;
+    case "DIVIDE":
+      if (roundB === 0) {
+        return "Error";
+      }
+      return roundA / roundB;
+    default:
+      throw new Error("Type must be SUM, SUBTRACT or DIVIDE");
   }
-};
+}
+
 module.exports = calculateNumber;
